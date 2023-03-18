49c59a058c034d44229bbb85478d5a825b560469
VGowthamTheja <vempalligowthamtheja@gmail.com>
2023-03-18 21:50:45 +0530

    refining polls app

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # change was_published_recently from boolean to icons on admin page
+    list_display_links = ('question_text', 'pub_date')
+    
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
